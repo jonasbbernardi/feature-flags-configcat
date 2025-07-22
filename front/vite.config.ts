71d972e12408d1d9fbb26244edede19ae3cdb777
Jonas B. Bernardi <jonas.bernardi@duosystem.com.br>
2025-07-22 15:11:39 -0300

    feat: vite dynamic port trough VITE_PORT env var

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'fs';
 import path, { dirname } from 'path';
@@ -10,7 +10,15 @@ const envPath = fs.existsSync(localEnvPath)
   ? localEnvPath
   : rootEnvPath;
 
-export default defineConfig({
-  plugins: [react()],
-  envDir: dirname(envPath)
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, dirname(envPath), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 5173,
+      host: true
+    },
+    envDir: dirname(envPath)
+  }
 })
